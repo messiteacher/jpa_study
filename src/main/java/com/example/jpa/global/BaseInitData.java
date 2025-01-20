bc9b7a858242e4f2ce83a8504a27b53c57b222c5
ShinYounHo <suh2963@gmail.com>
2025-01-20 13:58:13 +0900

    application run transaction apply

--- src/main/java/com/example/jpa/global/BaseInitData.java
+++ src/main/java/com/example/jpa/global/BaseInitData.java
@@ -3,10 +3,12 @@ package com.example.jpa.global;
 import com.example.jpa.domain.post.post.entity.Post;
 import com.example.jpa.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
@@ -36,11 +38,22 @@ public class BaseInitData {
     @Order(2)
     public ApplicationRunner applicationRunner2() {
 
-        return args -> {
-          Post post = postService.findById(1L).get();
-          Thread.sleep(1000);
+//        return args -> {
+//          Post post = postService.findById(1L).get();
+//          Thread.sleep(1000);
 //          postService.modify(post, "new title", "new body");
-          postService.modify2(1L, "new title", "new body");
+//          postService.modify2(1L, "new title", "new body");
+//        };
+
+        return new ApplicationRunner() {
+            @Override
+            @Transactional
+            public void run(ApplicationArguments args) throws Exception {
+
+                Post post = postService.findById(1L).get();
+                Thread.sleep(1000);
+                postService.modify(post, "new title", "new body");
+            }
         };
     }
 }
